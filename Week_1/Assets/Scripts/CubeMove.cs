52ae96806cc9b7cc56fa9b75a8fec82607ab6975
Meredith <meredityman@gmail.com>
2018-12-05 15:14:17 +0100

    Committing weeks 1 & 2

---
+++ Week_1/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform cubeTransform = GetComponent<Transform>(); 
+
+
+        Vector3 translation = new Vector3(0.0f, 0.4f, 0.0f); // Setting out translation
+        cubeTransform.Translate( translation ); // Doing translation
+        cubeTransform.Rotate(translation); // Doing rotation
+        
+    }
+}
---
+++ Week_2/Assets/Scripts/RobotSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSpawner : MonoBehaviour {
+
+    public GameObject robotPrefab;
+    public float spawnAreaSize = 2.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newRobot = Instantiate(robotPrefab, this.transform); // Instantiating new object
+
+
+            // Moving it in the x and z direction by a random amount
+            newRobot.transform.Translate(new Vector3(spawnAreaSize * Random.value, 0.0f, spawnAreaSize * Random.value)); 
+        }        
+    }
+}
